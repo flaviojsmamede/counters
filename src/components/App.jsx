525e8ba4f946f5a26312bc0ba5d36b0c0dc19382
Flavio Mamede <flaviojsmamede@gmail.com>
2023-09-27 17:42:18 +0100

    counters

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './Counter';
+import { useState } from "react";
 
 
 const App = () => {
+  const [counters, setCounters] = useState([
+    {id: 1, initial: 0, step: 1},
+    {id: 2, initial: 0, step: 1},
+    {id: 3, initial: 0, step: 1}
+  ]);
+
+  const handleAddCounter = () => {
+
+  }
+
+  const handleRemoveCounter = () => {
+
+  }
+
+
+
   return (
-    <div>
-    </div>
+  <div className="container m-5">
+    <button onClick="handleAddCounter" className='btn btn-primary'>Add counter</button>
+    {counters.map(({ id, ...props }) => {
+      return(
+      <div key={id}>
+        <Counter {...props} />
+        <button onClick="handleRemoveCounter" className='btn btn-outline-danger'>Remove counter <i class="fa-solid fa-trash-can"></i></button>
+      </div>
+      );
+    })}
+  </div>
   );
-}
+};
 
 export default App;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,25 @@
-const Counter = () => {
+import  { useState } from "react";
+
+const Counter = ({ initial, step = 1}) => {
+  const [count, setCount] = useState(initial);
+
+  const handleMinus = () => {
+    const newCount = count - step;
+
+    setCount(Math.max(newCount, 0));
+  }
+
+  const handlePlus = () => {
+    setCount(count + step);
+  }
+
   return (
-    <div>
-      <button>-</button>
-      <span>100</span>
-      <button>+</button>
+    <div className="m-3">
+      <button onClick={handleMinus} className="btn btn-outline-primary">-</button>
+      <span className="p-4 text-primary">{count}</span>
+      <button onClick={handlePlus} className="btn btn-outline-primary">+</button>
     </div>
-  )
-}
+  );
+};
 
 export default Counter;
